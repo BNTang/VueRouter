c786101b0ca57fca42e95acad72224f3b436f3cd
BNTang <303158131@qq.com>
2023-11-18 13:49:26 +0800

    first file

---
+++ 06-手撕Vue-Router-自定义路由出口组件/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/home">首页</router-link> |
+      <router-link to="/about">关于</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+  import Home from './views/Home'
+  import About from './views/About'
+  export default {
+    name: 'App',
+    components: {
+      Home,
+      About
+    },
+    mounted() {
+      console.log('App', this.$router);
+      console.log('App', this.$route);
+    }
+  }
+</script>
+<!--
+1.Vue-Router本质
+根据"不同的hash值"或者"不同的路径地址", 将不同的内容渲染到router-view中
+所以实现VueRouter的核心关键点就在于如何监听'hash'或'路径'的变化, 再将不同的内容写到router-view中
+-->
+<style>
+</style>
